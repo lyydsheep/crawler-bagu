bab1b9cd45fd065b264fbfffde04d65bc833b66b
lyydsheep <2230561977@qq.com>
2025-06-21 19:56:11 +0800

    add results

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "crawler-bagu/internal"
+import (
+	"crawler-bagu/internal"
+	"github.com/rs/zerolog/log"
+	"net/http"
+	_ "net/http/pprof"
+)
 
 func main() {
+	go func() {
+		if err := http.ListenAndServe(":6060", nil); err != nil {
+			log.Fatal().Err(err).Msg("http.ListenAndServe error")
+		}
+	}()
 	app := internal.InitializeApp()
 	app.Run()
 }
